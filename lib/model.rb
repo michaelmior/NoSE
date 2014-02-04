d9dcd3b31f2500311a4543482b5fb5eac029bc45
Michael Mior <mmior@uwaterloo.ca>
2014-02-04 13:43:27 -0500

    Add entity and field definition classes

---
+++ lib/model.rb
@@ -0,0 +1,73 @@
+class Entity
+ attr_reader :fields
+ attr_reader :name
+
+ def initialize(name)
+   @name = name
+   @fields = {}
+ end
+
+ def <<(field)
+   @fields[field.name] = field
+   self
+ end
+end
+
+class Field
+  attr_reader :name
+  attr_reader :type
+  attr_reader :size
+end
+
+class IntegerField < Field
+  def initialize(name)
+    @name = name
+    @type = :integer
+    @size = 8
+  end
+end
+
+class FloatField < Field
+  def initialize(name)
+    @name = name
+    @type = :float
+    @size = 8
+  end
+end
+
+class StringField < Field
+  def initialize(name, length)
+    @name = name
+    @type = :string
+    @size = length
+  end
+end
+
+
+class IDField < Field
+  def initialize(name)
+    @name = name
+    @type = :key
+    @size = 16
+  end
+end
+
+class ForeignKey < IDField
+  attr_reader :entity
+  attr_reader :cardinality
+
+  def initialize(name, entity)
+    super(name)
+    @cardinality = :one
+    @entity = entity
+  end
+end
+
+ToOneKey = ForeignKey
+
+class ToManyKey < ForeignKey
+  def initialize(name, entity)
+    super(name, entity)
+    @cardinality = :many
+  end
+end
---
+++ spec/model_spec.rb
@@ -0,0 +1,31 @@
+require_relative '../lib/model'
+
+describe Entity do
+  it 'can store fields' do
+    entity = Entity.new('Foo')
+    entity << IntegerField.new('Bar')
+    entity << IntegerField.new('Baz')
+
+    expect(entity.fields.keys).to match_array(['Bar', 'Baz'])
+  end
+
+  it 'can have foreign keys' do
+    entity = Entity.new('Foo')
+    other = Entity.new('Bar')
+    entity << ToOneKey.new('other', other)
+
+    expect(entity.fields['other'].entity).to be(other)
+    expect(entity.fields['other'].type).to eq(:key)
+    expect(entity.fields['other'].cardinality).to eq(:one)
+  end
+
+  it 'can have foreign keys with cardinality > 1' do
+    entity = Entity.new('Foo')
+    others = Entity.new('Bar')
+    entity << ToManyKey.new('others', others)
+
+    expect(entity.fields['others'].entity).to be(others)
+    expect(entity.fields['others'].type).to eq(:key)
+    expect(entity.fields['others'].cardinality).to eq(:many)
+  end
+end
