00b98ec34e7f5bf32c4037451237bf77e9750f10
Michael Mior <mmior@uwaterloo.ca>
2014-07-10 08:56:52 -0400

    Update RUBiS workload

--- workloads/rubis.rb
+++ workloads/rubis.rb
@@ -82,21 +82,28 @@ $workload = Sadvisor::Workload.new do
   end) * 1882
 
   # BrowseCategories
-  Q 'SELECT id, name FROM Category', 1.0
+  Q 'SELECT id, name FROM Category', 0.11
 
   # BrowseRegions
-  Q 'SELECT id, name FROM Region', 1.0
+  Q 'SELECT id, name FROM Region', (0.03 + 0.02)
 
   # ViewBidHistory
-  Q 'SELECT name FROM Item WHERE Item.id = ?', 1.0
-  Q 'SELECT name FROM OldItem WHERE OldItem.id = ?', 1.0
-  Q 'SELECT id, user_id, item_id, qty, bid, date FROM Bid WHERE Bid.Item.id = ? ORDER BY Bid.date DESC', 1.0
-  Q 'SELECT id, nickname FROM User WHERE User.Bid.Item.id = ?', 1.0
+  Q 'SELECT name FROM Item WHERE Item.id = ?', 0.02 / 4
+  Q 'SELECT name FROM OldItem WHERE OldItem.id = ?', 0.02 / 4
+  Q 'SELECT id, user_id, item_id, qty, bid, date FROM Bid WHERE Bid.Item.id = ? ORDER BY Bid.date DESC', 0.02 / 4
+  Q 'SELECT id, nickname FROM User WHERE User.Bid.Item.id = ?', 0.02 / 4
 
   # ViewItem
-  Q 'SELECT name FROM Item WHERE Item.id = ?', 1.0
-  Q 'SELECT name FROM OldItem WHERE OldItem.id = ?', 1.0
-  Q 'SELECT bid FROM Bid WHERE Bid.Item.id = ? ORDER BY Bid.bid DESC LIMIT 1', 1.0
-  Q 'SELECT bid, qty FROM Bid WHERE Bid.Item.id = ? ORDER BY Bid.bid DESC LIMIT 5', 1.0
-  Q 'SELECT id FROM Bid WHERE Bid.Item.id = ?', 1.0 # XXX: total bids
+  Q 'SELECT name FROM Item WHERE Item.id = ?', 0.12 / 4.0 * 0.75
+  Q 'SELECT name FROM OldItem WHERE OldItem.id = ?', 0.12 / 4.0 * 0.25
+  Q 'SELECT bid FROM Bid WHERE Bid.Item.id = ? ORDER BY Bid.bid DESC LIMIT 1', 0.12 / 4.0
+  Q 'SELECT bid, qty FROM Bid WHERE Bid.Item.id = ? ORDER BY Bid.bid DESC LIMIT 5', 0.12 / 4.0
+  Q 'SELECT id FROM Bid WHERE Bid.Item.id = ?', 0.12 / 4.0 # XXX: total bids
+
+  # SearchItemsByCategory
+  Q 'SELECT id, name, initial_price, max_bid, nb_of_bids, end_date FROM Item WHERE Item.Category.id = ? AND Item.end_date >= ?', (0.32 + 0.06)
+
+  # SearchItemsByRegion
+  # XXX Not supported
+  #Q 'SELECT id, name, initial_price, max_bid, nb_of_bids, end_date FROM Item WHERE Item.User.Region.id = ? AND Item.Category.id = ? AND Item.end_date >= ?', 0.06
 end
