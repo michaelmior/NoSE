071858431fd3205813c89f8eb53f72cf77f24988
Michael Mior <mmior@uwaterloo.ca>
2014-07-21 19:10:44 -0400

    Rename RUBiS entities

--- workloads/rubis.rb
+++ workloads/rubis.rb
@@ -2,17 +2,17 @@ $workload = Sadvisor::Workload.new do
   # Define entities along with the size and cardinality of their fields
   # as well as an estimated number of each entity
 
-  (Entity 'Category' do
+  (Entity 'categories' do
     ID     'id'
     String('name', 20) * 20
   end) * 20
 
-  (Entity 'Region' do
+  (Entity 'regions' do
     ID     'id'
     String('name', 25) * 62
   end) * 62
 
-  (Entity 'User' do
+  (Entity 'users' do
     ID         'id'
     String(    'firstname', 20) * 1001848
     String(    'lastname', 20) * 1001848
@@ -22,10 +22,10 @@ $workload = Sadvisor::Workload.new do
     Integer(   'rating') * 5
     Float(     'balance') * 1
     Date(      'creation_date') * 10381
-    ForeignKey('region', 'Region') * 62
+    ForeignKey('region', 'regions') * 62
   end) * 1001848
 
-  (Entity 'Item' do
+  (Entity 'items' do
     ID         'id'
     String(    'name', 100) * 33721
     String(    'description', 255) * 33721
@@ -37,11 +37,11 @@ $workload = Sadvisor::Workload.new do
     Float(     'max_bid') * 2167
     Date(      'start_date') * 1
     Date(      'end_date') * 1
-    ForeignKey 'seller', 'User'
-    ForeignKey 'category', 'Category'
+    ForeignKey 'seller', 'users'
+    ForeignKey 'category', 'categories'
   end) * 33721
 
-  (Entity 'OldItem' do
+  (Entity 'olditems' do
     ID         'id'
     String(    'name', 100) * 500000
     String(    'description', 255) * 500000
@@ -53,33 +53,33 @@ $workload = Sadvisor::Workload.new do
     Float(     'max_bid') * 5125
     Date(      'start_date') * 48436
     Date(      'end_date') * 239737
-    ForeignKey 'seller', 'User'
-    ForeignKey 'category', 'Category'
+    ForeignKey 'seller', 'users'
+    ForeignKey 'category', 'categories'
   end) * 500000
 
-  (Entity 'Bid' do
+  (Entity 'bids' do
     ID         'id'
-    ForeignKey('user_id', 'User') * 993655
-    ForeignKey('item_id', 'Item') * 426931
+    ForeignKey('user_id', 'users') * 993655
+    ForeignKey('item_id', 'items') * 426931
     Integer(   'qty') * 10
     Float(     'bid') * 5121
     Date(      'date') * 52913
   end) * 5060576
 
-  (Entity 'Comment' do
+  (Entity 'comments' do
     ID         'id'
-    ForeignKey('from_user_id', 'User') * 413603
-    # ForeignKey 'to_user_id', 'User'
-    ForeignKey('item_id', 'Item') * 443798
+    ForeignKey('from_user_id', 'users') * 413603
+    # ForeignKey 'to_user_id', 'users'
+    ForeignKey('item_id', 'items') * 443798
     Integer(   'rating') * 5
     Date(      'date') * 51399
     String(    'comment', 255) * 533426
   end) * 533426
 
-  (Entity 'BuyNow' do
+  (Entity 'buynow' do
     ID         'id'
-    ForeignKey('buyer_id', 'User') * 1519
-    ForeignKey('item_id', 'Item') * 1549
+    ForeignKey('buyer_id', 'users') * 1519
+    ForeignKey('item_id', 'items') * 1549
     Integer(   'qty') * 10
     Date(      'date') * 915
   end) * 1882
@@ -87,28 +87,28 @@ $workload = Sadvisor::Workload.new do
   # Define queries and their relative weights
 
   # BrowseCategories
-  Q 'SELECT id, name FROM Category', 0.11
+  Q 'SELECT id, name FROM categories', 0.11
 
   # BrowseRegions
-  Q 'SELECT id, name FROM Region', (0.03 + 0.02)
+  Q 'SELECT id, name FROM regions', (0.03 + 0.02)
 
   # ViewBidHistory
-  Q 'SELECT name FROM Item WHERE Item.id = ?', 0.02 / 4
-  Q 'SELECT name FROM OldItem WHERE OldItem.id = ?', 0.02 / 4
-  Q 'SELECT id, user_id, item_id, qty, bid, date FROM Bid WHERE Bid.item_id = ? ORDER BY Bid.date DESC', 0.02 / 4
-  Q 'SELECT id, nickname FROM User WHERE User.Bid.item_id = ?', 0.02 / 4
+  Q 'SELECT name FROM items WHERE items.id = ?', 0.02 / 4
+  Q 'SELECT name FROM olditems WHERE olditems.id = ?', 0.02 / 4
+  Q 'SELECT id, user_id, item_id, qty, bid, date FROM bids WHERE bids.item_id = ? ORDER BY bids.date DESC', 0.02 / 4
+  Q 'SELECT id, nickname FROM users WHERE users.bids.item_id = ?', 0.02 / 4
 
   # ViewItem
-  Q 'SELECT name FROM Item WHERE Item.id = ?', 0.12 / 4.0 * 0.75
-  Q 'SELECT name FROM OldItem WHERE OldItem.id = ?', 0.12 / 4.0 * 0.25
-  Q 'SELECT bid FROM Bid WHERE Bid.Item.id = ? ORDER BY Bid.bid DESC LIMIT 1', 0.12 / 4.0
-  Q 'SELECT bid, qty FROM Bid WHERE Bid.item_id = ? ORDER BY Bid.bid DESC LIMIT 5', 0.12 / 4.0
-  Q 'SELECT id FROM Bid WHERE Bid.item_id = ?', 0.12 / 4.0 # XXX: total bids
+  Q 'SELECT name FROM items WHERE items.id = ?', 0.12 / 4.0 * 0.75
+  Q 'SELECT name FROM olditems WHERE olditems.id = ?', 0.12 / 4.0 * 0.25
+  Q 'SELECT bid FROM bids WHERE bids.items.id = ? ORDER BY bids.bid DESC LIMIT 1', 0.12 / 4.0
+  Q 'SELECT bid, qty FROM bids WHERE bids.item_id = ? ORDER BY bids.bid DESC LIMIT 5', 0.12 / 4.0
+  Q 'SELECT id FROM bids WHERE bids.item_id = ?', 0.12 / 4.0 # XXX: total bids
 
   # SearchItemsByCategory
-  Q 'SELECT id, name, initial_price, max_bid, nb_of_bids, end_date FROM Item WHERE Item.category = ? AND Item.end_date >= ?', (0.32 + 0.06)
+  Q 'SELECT id, name, initial_price, max_bid, nb_of_bids, end_date FROM items WHERE items.category = ? AND items.end_date >= ?', (0.32 + 0.06)
 
   # XXX Not currently supported
   # # SearchItemsByRegion
-  # Q 'SELECT id, name, initial_price, max_bid, nb_of_bids, end_date FROM Item WHERE Item.User.region = ? AND Item.category = ? AND Item.end_date >= ?', 0.06
+  # Q 'SELECT id, name, initial_price, max_bid, nb_of_bids, end_date FROM items WHERE items.users.region = ? AND items.category = ? AND items.end_date >= ?', 0.06
 end
