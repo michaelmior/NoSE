e79c70bf1698c39297a2f4df9ea826a9d0bc8d20
Michael Mior <mmior@uwaterloo.ca>
2014-08-05 15:10:57 -0400

    Code cleanup for Rubocop

--- lib/sadvisor/planner.rb
+++ lib/sadvisor/planner.rb
@@ -465,9 +465,10 @@ module Sadvisor
       first_fields = first_fields.select do |field|
         field.parent == @path.first
       end
-      if first_fields == @path.first.id_fields && @path.length > 1
-        @path.shift
-      end
+
+      return unless first_fields == @path.first.id_fields && @path.length > 1
+
+      @path.shift
     end
   end
 
