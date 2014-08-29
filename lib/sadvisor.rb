4e1aef9b567b254fc3a7e8c3ee11cae385700ae2
Michael Mior <mmior@uwaterloo.ca>
2014-08-29 10:30:27 -0400

    Remove old parser

--- lib/sadvisor.rb
+++ lib/sadvisor.rb
@@ -12,6 +12,3 @@ require_relative 'sadvisor/planner'
 require_relative 'sadvisor/random'
 require_relative 'sadvisor/search'
 require_relative 'sadvisor/workload'
-
-# XXX Temporary separate file for new parser
-require_relative 'sadvisor/parslet'
