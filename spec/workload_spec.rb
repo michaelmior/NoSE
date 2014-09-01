9566749ad465062b1fc5474283ca0a407d80f621
Michael Mior <mmior@uwaterloo.ca>
2014-09-01 10:32:56 -0400

    Clean up explicit subject in tests

--- spec/workload_spec.rb
+++ spec/workload_spec.rb
@@ -1,39 +1,40 @@
 module Sadvisor
   describe Workload do
+    subject(:workload) { Workload.new}
     let(:entity)      { Entity.new('Foo') << field }
     let(:field)       { IDField.new('Id') }
 
     before(:each) do
-      subject.add_entity entity
+      workload.add_entity entity
     end
 
     it 'holds queries and entities' do
-      expect(subject.entities).to have(1).item
-      expect(subject.entities['Foo']).to be entity
+      expect(workload.entities).to have(1).item
+      expect(workload.entities['Foo']).to be entity
 
-      valid_query = Statement.new 'SELECT Id FROM Foo', subject
+      valid_query = Statement.new 'SELECT Id FROM Foo', workload
 
-      subject.add_query(valid_query)
-      expect(subject.queries).to match_array [valid_query]
+      workload.add_query(valid_query)
+      expect(workload.queries).to match_array [valid_query]
     end
 
     it 'can find fields on entities from queries' do
-      expect(subject.find_field %w(Foo Id)).to be field
+      expect(workload.find_field %w(Foo Id)).to be field
     end
 
     it 'can find fields which traverse foreign keys' do
       other_entity = Entity.new 'Bar'
       other_field = IDField.new 'Quux'
       other_entity << other_field
-      subject.add_entity other_entity
+      workload.add_entity other_entity
 
       entity << ForeignKey.new('Baz', other_entity)
 
-      expect(subject.find_field %w(Foo Baz Quux)).to be other_field
+      expect(workload.find_field %w(Foo Baz Quux)).to be other_field
     end
 
     it 'raises an exception for nonexistent entities' do
-      expect { subject['Bar'] }.to raise_error EntityNotFound
+      expect { workload['Bar'] }.to raise_error EntityNotFound
     end
   end
 end
