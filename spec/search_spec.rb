21a088325daed1e73612bf5baf09e8aa38c97b61
Michael Mior <mmior@uwaterloo.ca>
2014-09-01 22:53:43 -0400

    Add some extra search tests

--- spec/search_spec.rb
+++ spec/search_spec.rb
@@ -1,9 +1,33 @@
 module Sadvisor
   describe Search do
+    let(:workload) do
+      Workload.new do
+        Entity 'foo' do
+          ID 'id'
+          String 'bar'
+          Integer 'baz'
+        end
+      end
+    end
+
     it 'does nothing if no indexes are enumerated' do
-      workload = Workload.new
       indexes = Search.new(workload).search_overlap
       expect(indexes).to be_empty
+    end
+
+    it 'produces no indices if there is no space' do
+      simple_indexes = workload.entities.values.map(&:simple_index)
+      simple_size = simple_indexes.map(&:size).inject(0, &:+)
+      indexes = Search.new(workload).search_overlap(simple_size + 1)
+      expect(indexes).to be_empty
+    end
+
+    it 'produces a materialized view with sufficient space' do
+      query = Statement.new 'SELECT id FROM foo WHERE foo.bar = ? ' \
+                            'ORDER BY foo.baz', workload
+      workload.add_query query
+      indexes = Search.new(workload).search_overlap
+      expect(indexes).to include query.materialize_view
     end
   end
 end
