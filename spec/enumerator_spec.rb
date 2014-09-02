f4343fdfc55d0c30c6a03243e1f42610429d327f
Michael Mior <mmior@uwaterloo.ca>
2014-09-02 15:45:52 -0400

    Enumerator test cleanup

--- spec/enumerator_spec.rb
+++ spec/enumerator_spec.rb
@@ -1,58 +1,61 @@
 module Sadvisor
   describe IndexEnumerator do
-    before(:each) do
-      @w = w = Workload.new
+    subject(:enum) { IndexEnumerator.new workload }
+    let(:workload) do
+      Workload.new do
+        Entity 'Foo' do
+          ID 'Bar'
+          String 'Baz', 10
+        end
 
-      @w << Entity.new('Foo') do
-        ID 'Bar'
-        String 'Baz', 10
+        Entity 'Bar' do
+          ID 'Baz'
+          ForeignKey 'Quux', 'Foo'
+        end
       end
-
-      @w << Entity.new('Bar') do
-        ID 'Baz'
-        ForeignKey 'Quux', w['Foo']
-      end
-
     end
 
     it 'produces no indices for a simple select' do
-      enum = IndexEnumerator.new @w
-      query = Statement.new 'SELECT Bar FROM Foo', @w
+      query = Statement.new 'SELECT Bar FROM Foo', workload
       indexes = enum.indexes_for_query query
       expect(indexes).to be_empty
     end
 
     it 'produces a simple index for a filter' do
-      enum = IndexEnumerator.new @w
-      query = Statement.new 'SELECT Bar FROM Foo WHERE Foo.Baz = ?', @w
+      query = Statement.new 'SELECT Bar FROM Foo WHERE Foo.Baz = ?', workload
       indexes = enum.indexes_for_query query
-      expect(indexes).to have(1).item
-      expect(indexes.to_a).to include \
-        Index.new([@w['Foo']['Baz']], [], [@w['Foo']['Bar']], [@w['Foo']])
+
+      expect(indexes.to_a).to match_array [
+        Index.new([workload['Foo']['Baz']], [], [workload['Foo']['Bar']],
+                  [workload['Foo']])
+      ]
     end
 
     it 'produces a simple index for a foreign key join' do
-      enum = IndexEnumerator.new @w
-      query = Statement.new 'SELECT Baz FROM Bar WHERE Bar.Quux.Baz = ?', @w
+      query = Statement.new 'SELECT Baz FROM Bar WHERE Bar.Quux.Baz = ?',
+                            workload
       indexes = enum.indexes_for_query query
+
       expect(indexes).to include \
-        Index.new([@w['Foo']['Baz']], [], [@w['Bar']['Baz']], [@w['Bar'], @w['Foo']])
+        Index.new([workload['Foo']['Baz']], [], [workload['Bar']['Baz']],
+                  [workload['Bar'], workload['Foo']])
     end
 
     it 'produces a simple index for a filter within a workload' do
-      enum = IndexEnumerator.new @w
-      query = Statement.new 'SELECT Bar FROM Foo WHERE Foo.Baz = ?', @w
-      @w.add_query query
+      query = Statement.new 'SELECT Bar FROM Foo WHERE Foo.Baz = ?', workload
+      workload.add_query query
       indexes = enum.indexes_for_workload
-      expect(indexes).to have(1).item
-      expect(indexes.to_a).to include \
-        Index.new([@w['Foo']['Baz']], [], [@w['Foo']['Bar']], [@w['Foo']])
+
+      expect(indexes.to_a).to match_array [
+        Index.new([workload['Foo']['Baz']], [], [workload['Foo']['Bar']],
+                  [workload['Foo']])
+      ]
     end
 
     it 'does not produce empty indexes' do
-      enum = IndexEnumerator.new @w
-      query = Statement.new 'SELECT Baz FROM Bar WHERE Bar.Quux.Baz = ?', @w
-      @w.add_query query
+      query = Statement.new 'SELECT Baz FROM Bar WHERE Bar.Quux.Baz = ?',
+                            workload
+      workload.add_query query
       indexes = enum.indexes_for_workload
       expect(indexes).to all(satisfy do |index|
         !index.order_fields.empty? || !index.extra.empty?
