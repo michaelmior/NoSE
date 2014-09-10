a2e8f1ff7658abba551cd60c9e127f0d93930726
Michael Mior <mmior@uwaterloo.ca>
2014-09-10 16:05:27 -0400

    Add the option to execute index DDL

--- lib/sadvisor/backends/cassandra.rb
+++ lib/sadvisor/backends/cassandra.rb
@@ -1,36 +1,53 @@
+require 'cql'
+
 module Sadvisor
   # A backend which communicates with Cassandra via CQL
   class CassandraBackend
-    def initialize(workload, indexes, plans, **_options)
+    def initialize(workload, indexes, plans, **options)
       @workload = workload
       @indexes = indexes
       @plans = plans
+
+      @hosts = options[:backend]['hosts']
+      @port = options[:backend]['port']
+      @keyspace = options[:backend]['keyspace']
     end
 
     # Produce the DDL necessary for column families for the given indexes
-    def indexes_ddl
-      @indexes.map do |index|
-        fnames = lambda do |fields, type|
-          fields.map do |field|
-            name = '"' + field.name + '"'
-            name += ' ' + cassandra_type(field.class).to_s if type
-            name
-          end.join ', '
+    # and optionally execute them against the server
+    def indexes_ddl(execute = false)
+      Enumerator.new do |enum|
+        @indexes.map do |index|
+          fnames = lambda do |fields, type|
+            fields.map do |field|
+              name = '"' + field.name + '"'
+              name += ' ' + cassandra_type(field.class).to_s if type
+              name
+            end.join ', '
+          end
+
+          ddl = "CREATE COLUMNFAMILY \"#{index.key}\" (" \
+          "#{fnames.call index.all_fields, true}, " \
+          "PRIMARY KEY((#{fnames[index.hash_fields, false]})" \
+
+          ddl += ", #{fnames[index.order_fields, false]}" \
+            unless index.order_fields.empty?
+          ddl += '));'
+
+          enum.yield ddl
+          client.execute(ddl) if execute
         end
-
-        ddl = "CREATE COLUMNFAMILY \"#{index.key}\" (" \
-        "#{fnames.call index.all_fields, true}, " \
-        "PRIMARY KEY((#{fnames[index.hash_fields, false]})" \
-
-        ddl += ", #{fnames[index.order_fields, false]}" \
-          unless index.order_fields.empty?
-        ddl += '));'
-
-        ddl
       end
     end
 
     private
+
+    # Get a Cassandra client, connecting if not done already
+    def client
+      @client ||= Cql::Client.connect hosts: @hosts, port: @port.to_s,
+                                      keyspace: '"' + @keyspace + '"',
+                                      consistency: :one
+    end
 
     # Return the datatype to use in Cassandra for a given field
     def cassandra_type(field_class)
