87b8f88ab8d68a45f7e798657b5f842595f9bf56
Michael Mior <mmior@uwaterloo.ca>
2014-09-11 10:32:17 -0400

    Add progress bars for data loading

--- lib/sadvisor/cli.rb
+++ lib/sadvisor/cli.rb
@@ -102,13 +102,14 @@ module Sadvisor
     end
 
     desc 'load PLAN_FILE DIRECTORY', 'create indexes from the given PLAN_FILE'
+    option :progress, type: :boolean, default: true
     def load(plan_file, directory)
       result = load_results(plan_file)
       config = load_config
       backend = get_backend(config, result)
 
       loader = CSVLoader.new result.workload, backend
-      loader.load result.indexes, directory
+      loader.load result.indexes, directory, options[:progress]
     end
 
     private
