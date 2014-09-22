d9e26b3acd88be5212c7ff5a47d6782671919d01
Michael Mior <mmior@uwaterloo.ca>
2014-09-22 16:57:36 -0400

    Fix search tests for new ILP

--- spec/search_spec.rb
+++ spec/search_spec.rb
@@ -10,16 +10,9 @@ module Sadvisor
       end
     end
 
-    it 'does nothing if no indexes are enumerated' do
-      indexes = Search.new(workload).search_overlap
-      expect(indexes).to be_empty
-    end
-
-    it 'produces no indices if there is no space' do
-      simple_indexes = workload.entities.values.map(&:simple_index)
-      simple_size = simple_indexes.map(&:size).inject(0, &:+)
-      indexes = Search.new(workload).search_overlap(simple_size + 1)
-      expect(indexes).to be_empty
+    it 'raises an exception if there is no space' do
+      workload.add_query 'SELECT id FROM foo'
+      expect { Search.new(workload).search_overlap(1) }.to raise_error
     end
 
     it 'produces a materialized view with sufficient space' do
