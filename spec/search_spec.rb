97937a47550fb3fe5e66cc1ade9cc7db5f25107b
Michael Mior <mmior@uwaterloo.ca>
2014-09-26 18:23:06 -0400

    Allow multiple indexes per path step

--- spec/search_spec.rb
+++ spec/search_spec.rb
@@ -11,8 +11,24 @@ module Sadvisor
       query = Statement.new 'SELECT UserId FROM User WHERE User.City = ? ' \
                             'ORDER BY User.Username', workload
       workload.add_query query
+
       indexes = Search.new(workload).search_overlap
       expect(indexes).to include query.materialize_view
+    end
+
+    it 'can allow for multiple index lookups on one path segment' do
+      query = Statement.new 'SELECT Username FROM User WHERE User.City = ?',
+                            workload
+      workload.add_query query
+
+      indexes = [
+        Index.new([user['City']], [], [user['UserId']], [user]),
+        Index.new([user['UserId']], [], [user['Username']], [user])
+      ]
+      search = Search.new(workload)
+      expect do
+        search.search_overlap(indexes.first.size, indexes: indexes).to_set
+      end.to raise_error NoSolutionException
     end
   end
 end
