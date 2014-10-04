371b506cd2818af74ebf9403036966fb6e3a1227
Michael Mior <mmior@uwaterloo.ca>
2014-10-04 08:35:22 -0400

    Refactor workload command

--- lib/sadvisor/cli/workload.rb
+++ lib/sadvisor/cli/workload.rb
@@ -9,67 +9,75 @@ module Sadvisor
     option :format, type: :string, default: 'text'
     def workload(name)
       # rubocop:disable GlobalVars
-
-      is_text = options[:format] == 'text'
-
       require_relative "../../../workloads/#{name}"
+      workload = $workload
+      # rubocop:enable GlobalVars
 
-      enumerated_indexes = Sadvisor::IndexEnumerator.new($workload) \
+      enumerated_indexes = Sadvisor::IndexEnumerator.new(workload) \
         .indexes_for_workload.to_a
 
       if options[:max_space].finite?
-        indexes = Sadvisor::Search.new($workload) \
+        indexes = Sadvisor::Search.new(workload) \
           .search_overlap(enumerated_indexes, options[:max_space])
       else
         indexes = enumerated_indexes.clone
       end
 
-      planner = Sadvisor::Planner.new $workload, indexes
+      # Find the final plans for each query
+      planner = Sadvisor::Planner.new workload, indexes
       plans = {}
-      $workload.queries.each do |query|
+      workload.queries.map do |query|
         plans[query] = planner.min_plan query
       end
 
-      indexes = plans.values.map(&:to_a).flatten.select do |step|
+      # Get the indexes which are actually used
+      indexes = plans.map(&:to_a).flatten.select do |step|
         step.is_a? Sadvisor::IndexLookupPlanStep
       end.map(&:index).to_set
 
-      header = "Indexes\n" + '━' * 50
-      Formatador.display_line "[blue]#{header}[/]" if is_text
-      indexes.each do |index|
-        puts index.inspect
-      end if is_text
-      puts if is_text
+      result = OpenStruct.new(
+        workload: workload,
+        enumerated_indexes: enumerated_indexes,
+        indexes: indexes.to_set,
+        plans: plans.values,
+        total_size: indexes.map(&:size).inject(0, :+),
+        total_cost: workload.query_weights.map do |query, weight|
+          weight * plans[query].cost
+        end.inject(0, &:+)
+      )
 
-      total_size = indexes.map(&:size).inject(0, :+)
-      Formatador.display_line "Total size: [blue]#{total_size}[/]\n" if is_text
+      output_json result if options[:format] == 'json'
+      output_text result if options[:format] == 'text'
+    end
+
+    private
+
+    # Output the results of advising as text
+    def output_text(result)
+      # Output selected indexes
+      header = "Indexes\n" + '━' * 50
+      Formatador.display_line "[blue]#{header}[/]"
+      result.indexes.each do |index|
+        puts index.inspect
+      end
+
+      Formatador.display_line "Total size: [blue]#{result.total_size}[/]\n"
+      puts
 
       # Output queries plans for the discovered indices
       header = "Query plans\n" + '━' * 50
-      Formatador.display_line "[blue]#{header}[/]" if is_text
-      plans.each do |query, plan|
-        puts query.inspect if is_text
-        puts plan.inspect if is_text
-        puts if is_text
+      Formatador.display_line "[blue]#{header}[/]"
+      result.plans.each do |plan|
+        puts plan.query.inspect
+        puts plan.inspect
+        puts
       end
+    end
 
-      if options[:format] == 'json'
-        result = OpenStruct.new(
-          workload: $workload,
-          enumerated_indexes: enumerated_indexes,
-          indexes: indexes.to_set,
-          plans: plans.values,
-          total_size: total_size,
-          total_cost: $workload.query_weights.map do |query, weight|
-          weight * plans[query].cost
-          end.inject(0, &:+)
-        )
-
-        puts JSON.pretty_generate \
-          Sadvisor::SearchResultRepresenter.represent(result).to_hash
-      end
-
-      # rubocop:enable GlobalVars
+    # Output the results of advising as JSON
+    def output_json(result)
+      puts JSON.pretty_generate \
+        Sadvisor::SearchResultRepresenter.represent(result).to_hash
     end
   end
 end
