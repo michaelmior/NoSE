d8b22d912d602e13939f214caa819e0054300c2d
Michael Mior <mmior@uwaterloo.ca>
2014-10-17 11:32:18 -0400

    Add simple eBay workload

---
+++ workloads/ebay.rb
@@ -0,0 +1,36 @@
+# Insipired by the blog post below on data modeling in Cassandra
+# www.ebaytechblog.com/2012/07/16/cassandra-data-modeling-best-practices-part-1/
+
+# rubocop:disable all
+
+$workload = Sadvisor::Workload.new do
+  # Define entities along with the size and cardinality of their fields
+  # as well as an estimated number of each entity
+
+  Entity 'User' do
+    ID     'UserID'
+    String 'Name', 50
+    String 'Email', 50
+  end
+
+  Entity 'Item' do
+    ID     'ItemID'
+    String 'Title', 50
+    String 'Desc', 200
+  end
+
+  Entity 'Like' do
+    ID         'LikeID'
+    ForeignKey 'UserID', 'User'
+    ForeignKey 'ItemID', 'Item'
+    Date       'LikedAt'
+  end
+
+  # Define queries and their relative weights
+  Q 'SELECT * FROM User WHERE User.UserID = ?'
+  Q 'SELECT * FROM Item WHERE Item.ItemID = ?'
+  # Q 'SELECT * FROM Item WHERE Item.Like.ItemID = ?'
+  Q 'SELECT * FROM Item WHERE Item.Like.UserID = ? ORDER BY Item.Like.LikedAt'
+end
+
+# rubocop:enable all
