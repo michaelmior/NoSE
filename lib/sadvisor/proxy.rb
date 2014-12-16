878ee88c24d8b6fca794caba6a56ee3dc42acabe
Michael Mior <mmior@uwaterloo.ca>
2014-12-16 15:57:19 -0500

    Use EventMachine for the proxy

--- lib/sadvisor/proxy.rb
+++ lib/sadvisor/proxy.rb
@@ -1,3 +1,5 @@
+require 'eventmachine'
+
 module Sadvisor
   # A proxy server to interpret our query language and implement query plans
   class Proxy
@@ -5,24 +7,57 @@ module Sadvisor
       @result = result
       @backend = backend
       @config = config
+
+      @continue = true
     end
 
+    # Start the proxy server
     def start
-      return unless @server.nil?
+      socket = TCPServer.new('127.0.0.1', @config[:port])
+      socket.listen(100)
+      EventMachine.epoll
 
-      @server = TCPServer.new('127.0.0.1', @config[:port])
-      loop do
-        p @server
-        handle_connection @server.accept
+      EventMachine::run do
+        # Check when we need to shut down
+        EM.add_periodic_timer(4) do
+          EM.stop_event_loop unless @continue
+        end
+
+        # Start a new server
+        EM.watch(socket, ProxyServer, self) do |conn|
+          conn.proxy = self
+          conn.notify_readable = true
+        end
       end
     end
 
+    # Implemented by subclasses
     def handle_connection(_socket)
+      raise NotImplementedError
     end
 
+    # Stop accepting connections
     def stop
-      p 'STOP!'
-      @server.close if @server
+      @continue = false
+    end
+
+    private
+
+    # Simple connection subclass to pass things back up to the proxy
+    class ProxyServer < EM::Connection
+      attr_accessor :proxy
+
+      def notify_readable
+        while socket = @io.accept_nonblock
+          @proxy.handle_connection socket
+        end
+      rescue Errno::EAGAIN, Errno::ECONNABORTED
+      end
+
+      def unbind
+        detach
+        @io.close
+      end
     end
   end
 end
