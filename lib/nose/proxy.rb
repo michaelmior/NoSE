95d52b74262acf111277f17b8c739cf5df9dd4b3
Michael Mior <mmior@uwaterloo.ca>
2014-12-18 15:40:27 -0500

    Only select on write for new sockets

--- lib/nose/proxy.rb
+++ lib/nose/proxy.rb
@@ -19,16 +19,19 @@ module NoSE
       server_socket = TCPServer.new('127.0.0.1', @config[:port])
       server_socket.listen(100)
 
-      sockets = [server_socket]
+      read_sockets = [server_socket]
+      write_sockets = []
       loop do
-        read, write, error = IO.select sockets, sockets, sockets, 5
+        read, write, error = IO.select read_sockets, write_sockets,
+                                       read_sockets + write_sockets, 5
         break unless @continue
         next if read.nil? || write.nil? || error.nil?
 
         # Check if we have a new incoming connection
         if read.include? server_socket
           socket, _ = server_socket.accept
-          sockets << socket
+          read_sockets << socket
+          write_sockets << socket
           read.delete server_socket
         elsif error.include? server_socket
           @logger.error 'Server socket died'
@@ -37,11 +40,13 @@ module NoSE
 
         # Remove all sockets which have errors
         error.each { |socket| remove_connection socket }
-        sockets -= error
+        read_sockets -= error
+        write_sockets -= error
 
         # Handle connections on each available socket
         (read + write).each do |socket|
-          sockets.delete socket unless handle_connection socket
+          write_sockets.delete socket
+          read_sockets.delete socket unless handle_connection socket
         end
       end
     end
