b5f6eac52de29bb1469a0c1f252c26d0c3461294
Michael Mior <mmior@uwaterloo.ca>
2015-01-05 22:24:50 -0500

    Unify enumerator examples

--- spec/enumerator_spec.rb
+++ spec/enumerator_spec.rb
@@ -1,65 +1,45 @@
 module NoSE
   describe IndexEnumerator do
-    subject(:enum) { IndexEnumerator.new workload }
-    let(:workload) do
-      Workload.new do
-        Entity 'Foo' do
-          ID 'Bar'
-          String 'Baz', 10
-        end
+    include_context 'entities'
 
-        Entity 'Bar' do
-          ID 'Baz'
-          ForeignKey 'Quux', 'Foo'
-        end
-      end
-    end
+    subject(:enum) { IndexEnumerator.new workload }
 
     it 'produces a simple index for a filter' do
-      query = Query.new 'SELECT Bar FROM Foo WHERE Foo.Baz = ?', workload
+      query = Query.new 'SELECT Username FROM User WHERE User.City = ?',
+                        workload
       indexes = enum.indexes_for_query query
 
       expect(indexes.to_a).to include \
-        Index.new([workload['Foo']['Baz']], [], [workload['Foo']['Bar']],
-                  [workload['Foo']])
+        Index.new [user['City']], [], [user['Username']], [user]
     end
 
     it 'produces a simple index for a foreign key join' do
-      query = Query.new 'SELECT Baz FROM Bar.Quux WHERE Quux.Baz = ?',
+      query = Query.new 'SELECT Body FROM Tweet.User WHERE User.City = ?',
                         workload
       indexes = enum.indexes_for_query query
 
       expect(indexes).to include \
-        Index.new([workload['Foo']['Baz']], [], [workload['Bar']['Baz']],
-                  [workload['Foo'], workload['Bar']])
+        Index.new [user['City']], [], [tweet['Body']], [user, tweet]
     end
 
     it 'produces a simple index for a filter within a workload' do
-      query = Query.new 'SELECT Bar FROM Foo WHERE Foo.Baz = ?', workload
+      query = Query.new 'SELECT Username FROM User WHERE User.City = ?',
+                        workload
       workload.add_query query
       indexes = enum.indexes_for_workload
 
       expect(indexes.to_a).to include \
-        Index.new([workload['Foo']['Baz']], [], [workload['Foo']['Bar']],
-                  [workload['Foo']])
+        Index.new [user['City']], [], [user['Username']], [user]
     end
 
     it 'does not produce empty indexes' do
-      query = Query.new 'SELECT Baz FROM Bar.Quux WHERE Quux.Baz = ?', workload
+      query = Query.new 'SELECT Body FROM Tweet.User WHERE User.City = ?',
+                        workload
       workload.add_query query
       indexes = enum.indexes_for_workload
       expect(indexes).to all(satisfy do |index|
         !index.order_fields.empty? || !index.extra.empty?
       end)
-    end
-
-    it 'produces indices with multiple entities in the path' do
-      query = Query.new 'SELECT Baz FROM Bar.Quux WHERE Quux.Baz = ?', workload
-      workload.add_query query
-      indexes = enum.indexes_for_workload
-      expect(indexes).to include Index.new([workload['Foo']['Baz']], [],
-                                           [workload['Bar']['Baz']],
-                                           [workload['Foo'], workload['Bar']])
     end
   end
 end
