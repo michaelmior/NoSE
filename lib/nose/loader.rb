1c7945ea14d3b1f0c909c77826d2c05aa8ee1398
Michael Mior <mmior@uwaterloo.ca>
2015-01-17 14:51:17 -0500

    Add method definitions to loader base class

--- lib/nose/loader.rb
+++ lib/nose/loader.rb
@@ -5,5 +5,15 @@ module NoSE::Loader
       @workload = workload
       @backend = backend
     end
+
+    # Implemented by subclasses to load data for the given list of indexes
+    def load(indexes, config, show_progress = false)
+      raise NotImplementedError
+    end
+
+    # Implemented by subclasses to generate a model from the external source
+    def model(config)
+      raise NotImplementedError
+    end
   end
 end
