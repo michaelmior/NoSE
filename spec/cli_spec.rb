594d2ed5717457540377ba5cddc4a34a5b6ee191
Michael Mior <mmior@uwaterloo.ca>
2015-02-03 14:48:24 -0500

    Add graph output test

--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -1,9 +1,17 @@
+require 'graphviz'
+
 module NoSE::CLI
   describe NoSECLI do
     it 'can output help text' do
       run_simple 'nose help'
       expect(all_output).to start_with 'Commands:'
       expect(last_exit_status).to eq 0
+    end
+
+    it 'can output a graph of a workload' do
+      expect_any_instance_of(GraphViz).to \
+        receive(:output).with(png: '/tmp/rubis.png')
+      run_simple 'nose graph rubis /tmp/rubis.png'
     end
   end
 end
