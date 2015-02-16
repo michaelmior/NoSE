3b6948a6f2857930c86f21c809a4ed45fde9e650
Michael Mior <mmior@uwaterloo.ca>
2015-02-16 17:32:16 -0500

    Add CLI command to recost calculated plans

---
+++ lib/nose/cli/recost.rb
@@ -0,0 +1,26 @@
+module NoSE::CLI
+  # Add a command to generate a graphic of the schema from a workload
+  class NoSECLI < Thor
+    desc 'recost PLAN_FILE COST_MODEL',
+         'recost the workload in PLAN_FILE with COST_MODEL'
+    def recost(plan_file, cost_model)
+      result = load_results plan_file
+      config = load_config
+
+      # Get the new cost model and recost all the queries
+      new_cost_model = get_class 'cost', config[:cost_model][:name]
+      result.plans.each { |plan| plan.cost_model = new_cost_model }
+
+      plans = Hash[result.plans.map { |plan| [plan.query, plan] }]
+
+      result.cost_model = new_cost_model
+      total_cost = result.workload.statement_weights.map do |statement, weight|
+        next 0 unless statement.is_a? NoSE::Query
+        weight * plans[statement].cost
+      end.inject(0, &:+)
+      result.total_cost = total_cost
+
+      output_json result
+    end
+  end
+end
