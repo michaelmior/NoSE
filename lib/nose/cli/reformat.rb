c3b0fbc81feae51c4d9ba2ae5dd7e1cfd57a7f0d
Michael Mior <mmior@uwaterloo.ca>
2015-02-17 15:26:07 -0500

    Add reformat CLI command

---
+++ lib/nose/cli/reformat.rb
@@ -0,0 +1,14 @@
+module NoSE::CLI
+  # Add a command to reformat a plan file
+  class NoSECLI < Thor
+    desc 'reformat PLAN_FILE',
+         'reformat the data in PLAN_FILE'
+    option :format, type: :string, default: 'text', enum: ['text', 'json']
+    def reformat(plan_file)
+      result = load_results plan_file
+
+      # Output the results in the specified format
+      send(('output_' + options[:format]).to_sym, result)
+    end
+  end
+end
