55f26c7361f7070c8a078fa431544ef413c369a1
Michael Mior <mmior@uwaterloo.ca>
2015-02-23 16:42:22 -0500

    Fix possible duplicate index fields

--- lib/nose/enumerator.rb
+++ lib/nose/enumerator.rb
@@ -128,11 +128,11 @@ module NoSE
         indexes = []
 
         order_choices.each do |order|
-          # Skip indices with only a hash component
-          index_extra = extra - (index + order)
-
           # Append the primary key of the last entity in the path if needed
           order += path.last.id_fields - (index + order)
+
+          # Skip indices with only a hash component
+          index_extra = extra - (index + order)
 
           next if order.empty? && index_extra.empty?
 
