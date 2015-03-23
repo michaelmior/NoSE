94c0129baa942da935088b3b3226b120f3ea8706
Michael Mior <mmior@uwaterloo.ca>
2015-03-23 09:06:41 -0400

    Add console command for easy debugging

---
+++ lib/nose/cli/console.rb
@@ -0,0 +1,26 @@
+require 'pry'
+
+module NoSE::CLI
+  # Start a pry console while preloading configured objects
+  class NoSECLI < Thor
+    desc 'console PLAN_FILE', 'open a pry console preconfigured with ' \
+                              'variables from the given PLAN_FILE'
+    def console(plan_file)
+      # Load the results from the plan file and define each as a variable
+      result = load_results plan_file
+      result.each_pair do |name, value|
+        TOPLEVEL_BINDING.local_variable_set name, value
+      end
+
+      # Also extract the model as a variable
+      TOPLEVEL_BINDING.local_variable_set :model, result.workload.model
+
+      # Load the config and backend as variables
+      config = load_config
+      TOPLEVEL_BINDING.local_variable_set :config, config
+      TOPLEVEL_BINDING.local_variable_set :backend, get_backend(config, result)
+
+      TOPLEVEL_BINDING.pry
+    end
+  end
+end
