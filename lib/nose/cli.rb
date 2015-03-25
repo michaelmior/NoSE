41fabf7222858164a5cd5ee72867ee223e0a0f06
Michael Mior <mmior@uwaterloo.ca>
2015-03-25 11:29:01 -0400

    Refactor search in CLI

--- lib/nose/cli.rb
+++ lib/nose/cli.rb
@@ -56,6 +56,37 @@ module NoSE::CLI
       end
     end
 
+    # Collect all advisor results for schema design problem
+    def search(workload, cost_model, max_space)
+      enumerated_indexes = NoSE::IndexEnumerator.new(workload) \
+        .indexes_for_workload.to_a
+      indexes = find_indexes workload, enumerated_indexes, max_space,
+        cost_model
+
+      # Find the final plans for each query
+      planner = NoSE::Plans::QueryPlanner.new workload, indexes, cost_model
+      plans = {}
+      workload.queries.each { |query| plans[query] = planner.min_plan query }
+
+      # Get the indexes which are actually used
+      indexes = plans.map(&:to_a).flatten.select do |step|
+        step.is_a? NoSE::Plans::IndexLookupPlanStep
+      end.map(&:index).to_set
+
+      return OpenStruct.new(
+        workload: workload,
+        enumerated_indexes: enumerated_indexes,
+        indexes: indexes.to_set,
+        plans: plans.values,
+        cost_model: cost_model,
+        total_size: indexes.map(&:size).inject(0, :+),
+        total_cost: workload.statement_weights.map do |statement, weight|
+          next 0 unless statement.is_a? NoSE::Query
+          weight * plans[statement].cost
+        end.inject(0, &:+)
+      )
+    end
+
     # Load results of a previous search operation
     def load_results(plan_file)
       representer = NoSE::Serialize::SearchResultRepresenter.represent \
