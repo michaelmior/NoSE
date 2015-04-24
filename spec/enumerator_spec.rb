5360eb5e922cd0c6563c6bf580d0c3e1c9f87b9d
Michael Mior <mmior@uwaterloo.ca>
2015-04-24 13:18:23 -0400

    Add index enumeration for support queries

--- spec/enumerator_spec.rb
+++ spec/enumerator_spec.rb
@@ -47,6 +47,19 @@ module NoSE
       end)
     end
 
+    it 'includes no indexes for updates if nothing is updated' do
+      # Use a fresh workload for this test
+      model = workload.model
+      workload = Workload.new model
+      enum = IndexEnumerator.new workload
+      update = Update.new 'UPDATE User SET Username = ? WHERE User.City = ?',
+               model
+      workload.add_statement update
+      indexes = enum.indexes_for_workload
+
+      expect(indexes).to be_empty
+    end
+
     it 'includes indexes enumerated from queries generated from updates' do
       # Use a fresh workload for this test
       model = workload.model
@@ -56,10 +69,15 @@ module NoSE
       update = Update.new 'UPDATE User SET Username = ? WHERE User.City = ?',
                           model
       workload.add_statement update
+
+      query = Query.new 'SELECT Body FROM Tweet.User WHERE User.Username = ?',
+                        workload.model
+      workload.add_statement query
+
       indexes = enum.indexes_for_workload
 
       expect(indexes.to_a).to include \
-        Index.new [user['City']], [user['UserId']], [user['UserId']], [user]
+        Index.new [user['City']], [user['UserId']], [user['Username']], [user]
     end
   end
 end
