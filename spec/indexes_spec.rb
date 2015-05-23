e6414e85c191964e38f47795f528f83291f7404e
Michael Mior <mmior@uwaterloo.ca>
2015-05-23 16:28:51 -0400

    Change index tests to use common examples

--- spec/indexes_spec.rb
+++ spec/indexes_spec.rb
@@ -1,32 +1,18 @@
 module NoSE
   describe Index do
-    let(:workload) do
-      Workload.new do
-        Entity 'Corge' do
-          ID 'Id'
-          Integer 'Quux'
-        end
+    include_context 'entities'
 
-        (Entity 'Foo' do
-          ID 'Id'
-          Integer 'Bar'
-        end) * 100
-
-        HasOne 'Corge', 'Foo',
-               'Foo' => 'Corge'
-      end
-    end
-    let(:model) { workload.model }
     let(:equality_query) do
-      Query.new 'SELECT Foo.Bar FROM Foo WHERE Foo.Id = ?', model
+      Query.new 'SELECT Tweet.Body FROM Tweet WHERE Tweet.TweetId = ?',
+                workload.model
     end
     let(:combo_query) do
-      Query.new 'SELECT Foo.Id FROM Foo WHERE Foo.Id > ? AND Foo.Bar = ?',
-                model
+      Query.new 'SELECT Tweet.Body FROM Tweet WHERE Tweet.Timestamp > ? ' \
+                'AND Tweet.TweetId = ?', workload.model
     end
     let(:order_query) do
-      Query.new 'SELECT Foo.Id FROM Foo WHERE Foo.Id = ? ORDER BY Foo.Bar',
-                model
+      Query.new 'SELECT Tweet.Body FROM Tweet WHERE Tweet.TweetId = ? ' \
+                'ORDER BY Tweet.Timestamp', workload.model
     end
 
     before(:each) do
@@ -36,103 +22,102 @@ module NoSE
     end
 
     it 'contains fields' do
-      index = Index.new [model['Foo']['Id']], [], [model['Foo']['Bar']],
-                        [model['Foo'].id_fields.first]
-      expect(index.contains_field? model['Foo']['Id']).to be true
+      index = Index.new [tweet['TweetId']], [], [tweet['Body']],
+                        [tweet.id_fields.first]
+      expect(index.contains_field? tweet['TweetId']).to be true
     end
 
     it 'can store additional fields' do
-      index = Index.new [model['Foo']['Id']], [], [model['Foo']['Bar']],
-                        [model['Foo'].id_fields.first]
-      expect(index.contains_field? model['Foo']['Bar']).to be true
+      index = Index.new [tweet['TweetId']], [], [tweet['Body']],
+                        [tweet.id_fields.first]
+      expect(index.contains_field? tweet['Body']).to be true
     end
 
     it 'can calculate its size' do
-      index = Index.new [model['Foo']['Id']], [], [model['Foo']['Bar']],
-                        [model['Foo'].id_fields.first]
-      entry_size = model['Foo']['Id'].size + model['Foo']['Bar'].size
+      index = Index.new [tweet['TweetId']], [], [tweet['Body']],
+                        [tweet.id_fields.first]
+      entry_size = tweet['TweetId'].size + tweet['Body'].size
       expect(index.entry_size).to eq(entry_size)
-      expect(index.size).to eq(entry_size * model['Foo'].count)
+      expect(index.size).to eq(entry_size * tweet.count)
     end
 
     context 'when materializing views' do
       it 'supports equality predicates' do
         index = equality_query.materialize_view
-        expect(index.hash_fields).to eq([model['Foo']['Id']].to_set)
+        expect(index.hash_fields).to eq([tweet['TweetId']].to_set)
       end
 
       it 'support range queries' do
         index = combo_query.materialize_view
-        expect(index.order_fields).to eq([model['Foo']['Id']])
+        expect(index.order_fields).to eq([tweet['Timestamp']])
       end
 
       it 'supports multiple predicates' do
         index = combo_query.materialize_view
-        expect(index.hash_fields).to eq([model['Foo']['Bar']].to_set)
-        expect(index.order_fields).to eq([model['Foo']['Id']])
+        expect(index.hash_fields).to eq([tweet['TweetId']].to_set)
+        expect(index.order_fields).to eq([tweet['Timestamp']])
       end
 
       it 'supports order by' do
         index = order_query.materialize_view
-        expect(index.order_fields).to eq([model['Foo']['Bar']])
+        expect(index.order_fields).to eq([tweet['Timestamp']])
       end
 
       it 'keeps a static key' do
         index = combo_query.materialize_view
-        expect(index.key).to eq 'i3087462560'
+        expect(index.key).to eq 'i3932123199'
       end
 
       it 'includes only one entity in the hash fields' do
-        query = Query.new 'SELECT Foo.Id FROM Foo.Corge WHERE Foo.Bar = ? ' \
-                          'AND Corge.Quux = ?', model
+        query = Query.new 'SELECT Tweet.TweetId FROM Tweet.User ' \
+                          'WHERE Tweet.Timestamp = ? AND User.City = ?',
+                          workload.model
         index = query.materialize_view
         expect(index.hash_fields.map(&:parent).uniq).to have(1).item
       end
     end
 
     it 'can tell if it maps identities for a field' do
-      index = Index.new [model['Foo']['Id']], [], [model['Foo']['Bar']],
-                        [model['Foo'].id_fields.first]
-      expect(index.identity_for? model['Foo']).to be true
+      index = Index.new [tweet['TweetId']], [], [tweet['Body']],
+                        [tweet.id_fields.first]
+      expect(index.identity_for? tweet).to be true
     end
 
     it 'can be created to map entity fields by id' do
-      index = model['Foo'].simple_index
-      expect(index.hash_fields).to eq([model['Foo']['Id']].to_set)
+      index = tweet.simple_index
+      expect(index.hash_fields).to eq([tweet['TweetId']].to_set)
       expect(index.order_fields).to eq([])
-      expect(index.extra).to eq([
-        model['Foo']['Bar']
-      ].to_set)
-      expect(index.key).to eq 'Foo'
+      expect(index.extra).to eq([tweet['Body'], tweet['Timestamp']].to_set)
+      expect(index.key).to eq 'Tweet'
     end
 
     context 'when checking validity' do
       it 'cannot have empty hash fields' do
         expect do
-          Index.new [], [], [model['Foo']['Id']],
-                    [model['Foo'].id_fields.first]
+          Index.new [], [], [tweet['TweetId']],
+                    [tweet.id_fields.first]
         end.to raise_error
       end
 
       it 'cannot have hash fields involving multiple entities' do
         expect do
-          Index.new [model['Foo']['Bar'], model['Corge']['Quux']],
-                    [model['Foo']['Id']], [],
-                    [model['Corge'].id_fields.first, model['Foo']['Corge']]
+          Index.new [tweet['Body'], user['City']],
+                    tweet.id_fields + user.id_fields, [],
+                    [tweet.id_fields.first, tweet['User']]
         end.to raise_error
       end
 
       it 'must have fields at the start of the path' do
         expect do
-          Index.new [model['Foo']['Id']], [], [],
-                    [model['Corge'].id_fields.first, model['Foo']['Corge']]
+          Index.new [tweet['TweetId']], [], [],
+                    [tweet.id_fields.first, tweet['User']]
         end.to raise_error InvalidIndexException
       end
 
       it 'must have fields at the end of the path' do
         expect do
-          Index.new [model['Corge']['Quux']], [], [],
-                    [model['Corge'].id_fields.first, model['Foo']['Corge']]
+          Index.new [user['City']], [], [],
+                    [tweet.id_fields.first, tweet['User']]
         end.to raise_error InvalidIndexException
       end
     end
