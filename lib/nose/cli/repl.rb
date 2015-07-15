049a9a1c3117b47790181732f6e4c6a1b5c1b709
Michael Mior <mmior@uwaterloo.ca>
2015-07-15 17:46:52 -0400

    Save history in REPL

--- lib/nose/cli/repl.rb
+++ lib/nose/cli/repl.rb
@@ -15,6 +15,18 @@ module NoSE
         result = load_results plan_file
         backend = get_backend(options, result)
 
+        # Load the history file
+        history_file = File.expand_path '~/.nose_history'
+        if Object.const_defined? 'Readline'
+          begin
+            File.foreach(history_file) do |line|
+              Readline::HISTORY.push line.chomp
+            end
+          rescue Errno::ENOENT
+            nil
+          end
+        end
+
         loop do
           begin
             line = read_line
@@ -23,10 +35,18 @@ module NoSE
           end
           break if line.nil?
 
-          line.chomp!
           next if line.empty?
 
           execute_statement line, result, backend
+        end
+
+        # Save the history file
+        if Object.const_defined? 'Readline'
+          File.open(history_file, 'w') do |f|
+            Readline::HISTORY.each do |line|
+              f.puts line
+            end
+          end
         end
       end
 
@@ -40,13 +60,13 @@ module NoSE
           line = Readline.readline prefix
           return if line.nil?
 
-          Readline::HISTORY.push line
+          Readline::HISTORY.push line unless line.chomp.empty?
         else
           print prefix
           line = gets
         end
 
-        line
+        line.chomp
       end
 
       # Parse a statement from a given string of text
