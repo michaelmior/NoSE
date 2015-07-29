e23050a026cb614fd2eb3a6d9cc7df1ac295deba
Michael Mior <mmior@uwaterloo.ca>
2015-07-29 14:23:12 -0400

    Add DSL for specifying execution plans

---
+++ lib/nose/execution_plan.rb
@@ -0,0 +1,85 @@
+module NoSE
+  # Simple DSL for constructing execution plans
+  class ExecutionPlans
+    attr_reader :plans, :schema
+
+    def initialize(&block)
+      @plans = []
+      instance_eval(&block) if block_given?
+    end
+
+    # rubocop:disable MethodName
+
+    # Set the schema to be used by the execution plans
+    def Schema(name)
+      @schema = Schema.load name
+      NoSE::DSL.mixin_fields @schema.workload.model.entities,
+                             QueryExecutionPlan
+    end
+
+    # Define a group of query execution plans
+    def Group(_name, _weight = 1.0, **_mixes, &block)
+      # XXX Groups are basically ignored for now
+      instance_eval(&block) if block_given?
+    end
+
+    # Define a single plan within a group
+    def Plan(&block)
+      return unless block_given?
+
+      plan = QueryExecutionPlan.new(@schema)
+      plan.instance_eval(&block)
+      @plans << plan
+    end
+
+    # rubocop:enable MethodName
+  end
+
+  # DSL to construct query execution plans
+  class QueryExecutionPlan
+    attr_reader :params, :select, :steps
+
+    def initialize(schema)
+      @schema = schema
+      @select = []
+      @params = []
+      @steps = []
+    end
+
+    # rubocop:disable MethodName
+
+    # Identify fields to be selected
+    def Select(*fields)
+      @select = fields
+    end
+
+    # Add parameters which are used as input to the plan
+    def Param(field, operator, value = nil)
+      operator = :'=' if operator == :==
+      @params << Condition.new(field, operator, value)
+    end
+
+    # Create a new index lookup step with a particular set of conditions
+    def Lookup(index_key, *conditions)
+      index = @schema.indexes[index_key]
+
+      step = Plans::IndexLookupPlanStep.new index
+      eq_fields = []
+      range_field = nil
+      conditions.each do |field, operator|
+        if operator == :==
+          eq_fields.push field
+        else
+          range_field = field
+        end
+      end
+
+      step.instance_variable_set :@eq_filter, eq_fields
+      step.instance_variable_set :@range_filter, range_field
+
+      @steps << step
+    end
+
+    # rubocop:enable MethodName
+  end
+end
