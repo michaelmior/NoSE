f84a9ecd5f32b0d7376217b6d6a62a4b3b2a589a
Michael Mior <mmior@uwaterloo.ca>
2015-07-29 16:38:55 -0400

    Allow create and load to work with manual schemas

--- lib/nose/cli/create.rb
+++ lib/nose/cli/create.rb
@@ -2,12 +2,19 @@ module NoSE
   module CLI
     # Add a command for creating the index data structures in a backend
     class NoSECLI < Thor
-      desc 'create PLAN_FILE', 'create indexes from the given PLAN_FILE'
+      desc 'create PLAN_FILE_OR_SCHEMA', 'create indexes from the given PLAN_FILE_OR_SCHEMA'
       option :dry_run, type: :boolean, default: false
       option :skip_existing, type: :boolean, default: false, aliases: '-s'
       option :drop_existing, type: :boolean, default: false, aliases: '-d'
       def create(plan_file)
-        result = load_results(plan_file)
+        if File.exist? plan_file
+          result = load_results(plan_file)
+        else
+          schema = Schema.load plan_file
+          result = OpenStruct.new
+          result.workload = schema.workload
+          result.indexes = schema.indexes.values
+        end
         backend = get_backend(options, result)
 
         # Produce the DDL and execute unless the dry run option was given
