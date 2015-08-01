b9198cd12b07b211a7518b11b1ce51c627131a96
Michael Mior <mmior@uwaterloo.ca>
2015-08-01 12:36:57 -0400

    Add extra tests for type matching in MySQL loader

--- spec/loader/mysql_loader_spec.rb
+++ spec/loader/mysql_loader_spec.rb
@@ -31,7 +31,11 @@ module NoSE::Loader
         'SELECT COUNT(*) FROM Foo' => [{ 'COUNT()*)' => 10 }],
         'DESCRIBE Foo' => EachArray.new([
           ['FooId', 'int(10) unsigned', 'NO', 'PRI', 'NULL', ''],
-          ['Bar', 'int(10) unsigned', 'NO', '', 'NULL', '']
+          ['Bar', 'int(10) unsigned', 'NO', '', 'NULL', ''],
+          ['Baz', 'float', 'NO', '', 'NULL', ''],
+          ['Quux', 'datetime', 'NO', '', 'NULL', ''],
+          ['Corge', 'text', 'NO', '', 'NULL', ''],
+          ['Garply', 'varchar(10)', 'NO', '', 'NULL', '']
         ])
       }, 3)
 
@@ -40,7 +44,14 @@ module NoSE::Loader
 
       entity = workload.model.entities.values.first
       expect(entity.name).to eq 'Foo'
-      expect(entity.fields).to have(2).items
+      expect(entity.fields).to have(6).items
+
+      expect(entity.fields.values[0]).to be_a NoSE::Fields::IDField
+      expect(entity.fields.values[1]).to be_a NoSE::Fields::IntegerField
+      expect(entity.fields.values[2]).to be_a NoSE::Fields::FloatField
+      expect(entity.fields.values[3]).to be_a NoSE::Fields::DateField
+      expect(entity.fields.values[4]).to be_a NoSE::Fields::StringField
+      expect(entity.fields.values[5]).to be_a NoSE::Fields::StringField
     end
   end
 end
