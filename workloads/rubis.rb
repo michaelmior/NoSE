adb921a9f2be8d2c9806ac85ecb575aa72f72a26
Michael Mior <mmior@uwaterloo.ca>
2015-08-11 11:21:19 -0400

    Add comments to tag RUBiS queries

--- workloads/rubis.rb
+++ workloads/rubis.rb
@@ -9,28 +9,28 @@ NoSE::Workload.new do
   DefaultMix :browsing
 
   Group 'BrowseCategories', browsing: 4.44 + 3.21, bidding: 7.65 + 5.39 do
-    Q 'SELECT users.nickname, users.password FROM users WHERE users.id = ?'
+    Q 'SELECT users.nickname, users.password FROM users WHERE users.id = ? -- 1'
     # XXX Must have at least one equality predicate
     Q 'SELECT categories.id, categories.name FROM categories WHERE ' \
-      'categories.dummy = 1'
+      'categories.dummy = 1 -- 2'
   end
 
   Group 'ViewBidHistory', browsing: 2.38, bidding: 1.54 do
-    Q 'SELECT items.name FROM items WHERE items.id = ?'
+    Q 'SELECT items.name FROM items WHERE items.id = ? -- 3'
     Q 'SELECT users.id, users.nickname, bids.id, item.id, bids.qty, ' \
       'bids.bid, bids.date FROM users.bids.item WHERE item.id = ? ' \
-      'ORDER BY bids.date'
+      'ORDER BY bids.date -- 4'
   end
 
   Group 'ViewItem', browsing: 22.95, bidding: 14.17 do
-    Q 'SELECT items.* FROM items WHERE items.id = ?'
-    Q 'SELECT bids.* FROM items.bids WHERE items.id = ?'
+    Q 'SELECT items.* FROM items WHERE items.id = ? -- 5'
+    Q 'SELECT bids.* FROM items.bids WHERE items.id = ? -- 6'
   end
 
   Group 'SearchItemsByCategory', browsing: 27.77 + 8.26, bidding: 15.94 + 6.34 do
     Q 'SELECT items.id, items.name, items.initial_price, items.max_bid, ' \
       'items.nb_of_bids, items.end_date FROM items.category WHERE ' \
-      'category.id = ? AND items.end_date >= ? LIMIT 25'
+      'category.id = ? AND items.end_date >= ? LIMIT 25 -- 7'
   end
 
   # XXX Not currently supported
@@ -43,75 +43,74 @@ NoSE::Workload.new do
 
   Group 'ViewUserInfo', browsing: 4.41, bidding: 2.48 do
     # XXX Not including region name below
-    Q 'SELECT users.* FROM users WHERE users.id = ?'
+    Q 'SELECT users.* FROM users WHERE users.id = ? -- 8'
     Q 'SELECT comments.id, comments.rating, comments.date, comments.comment ' \
-      'FROM comments.to_user WHERE to_user.id = ?'
+      'FROM comments.to_user WHERE to_user.id = ? -- 9'
   end
 
   Group 'RegisterItem', bidding: 0.53 do
     Q 'INSERT INTO items SET id=?, name=?, description=?, initial_price=?, ' \
       'quantity=?, reserve_price=?, buy_now=?, nb_of_bids=0, max_bid=0, ' \
-      'start_date=?, end_date=? AND CONNECT TO category(?), seller(?)'
+      'start_date=?, end_date=? AND CONNECT TO category(?), seller(?) -- 10'
   end
 
   Group 'RegisterUser', bidding: 1.07 do
     Q 'INSERT INTO users SET id=?, firstname=?, lastname=?, nickname=?, ' \
       'password=?, email=?, rating=0, balance=0, creation_date=? ' \
-      'AND CONNECT TO region(?)'
+      'AND CONNECT TO region(?) -- 11'
   end
 
   Group 'BuyNow', bidding: 1.16 do
-    Q 'SELECT users.nickname FROM users WHERE users.id=?'
-    Q 'SELECT items.* FROM items WHERE items.id=?'
+    Q 'SELECT users.nickname FROM users WHERE users.id=? -- 12'
+    Q 'SELECT items.* FROM items WHERE items.id=? -- 13'
   end
 
   Group 'StoreBuyNow', bidding: 1.10 do
     Q 'SELECT items.quantity, items.nb_of_bids, items.end_date FROM items ' \
-      'WHERE items.id=?'
-    Q 'UPDATE items SET quantity=?, nb_of_bids=?, end_date=? WHERE items.id=?'
+      'WHERE items.id=? -- 14'
+    Q 'UPDATE items SET quantity=?, nb_of_bids=?, end_date=? WHERE items.id=? -- 15'
     Q 'INSERT INTO buynow SET id=?, qty=?, date=? ' \
-      'AND CONNECT TO item(?), buyer(?)'
+      'AND CONNECT TO item(?), buyer(?) -- 16'
   end
 
   Group 'PutBid', bidding: 5.40 do
-    Q 'SELECT users.nickname, users.password FROM users WHERE users.id=?'
-    Q 'SELECT items.* FROM items WHERE items.id=?'
+    Q 'SELECT users.nickname, users.password FROM users WHERE users.id=? -- 17'
+    Q 'SELECT items.* FROM items WHERE items.id=? -- 18'
     Q 'SELECT bids.qty, bids.date FROM bids.item WHERE item.id=? ' \
-      'ORDER BY bids.bid LIMIT 2'
+      'ORDER BY bids.bid LIMIT 2 -- 19'
   end
 
   Group 'StoreBid', bidding: 3.74 do
     Q 'INSERT INTO bids SET id=?, qty=?, bid=?, date=? ' \
-      'AND CONNECT TO item(?), user(?)'
-    Q 'SELECT items.nb_of_bids, items.max_bid FROM items WHERE items.id=?'
-    Q 'UPDATE items SET nb_of_bids=?, max_bid=? WHERE items.id=?'
+      'AND CONNECT TO item(?), user(?) -- 20'
+    Q 'SELECT items.nb_of_bids, items.max_bid FROM items WHERE items.id=? -- 21'
+    Q 'UPDATE items SET nb_of_bids=?, max_bid=? WHERE items.id=? -- 22'
   end
 
   Group 'PutComment', bidding: 0.46 do
-    Q 'SELECT users.nickname, users.password FROM users WHERE users.id=?'
-    Q 'SELECT items.* FROM items WHERE items.id=?'
-    Q 'SELECT users.* FROM users WHERE users.id=?'
+    Q 'SELECT users.nickname, users.password FROM users WHERE users.id=? -- 23'
+    Q 'SELECT items.* FROM items WHERE items.id=? -- 24'
+    Q 'SELECT users.* FROM users WHERE users.id=? -- 25'
   end
 
   Group 'StoreComment', bidding: 0.45 do
-    Q 'SELECT users.rating FROM users WHERE users.id=?'
-    Q 'UPDATE users SET rating=? WHERE users.id=?'
+    Q 'SELECT users.rating FROM users WHERE users.id=? -- 26'
+    Q 'UPDATE users SET rating=? WHERE users.id=? -- 27'
     Q 'INSERT INTO comments SET id=?, rating=?, date=?, comment=? ' \
-      'AND CONNECT TO to_user(?), from_user(?), item(?)'
+      'AND CONNECT TO to_user(?), from_user(?), item(?) -- 28'
   end
 
   Group 'AboutMe', bidding: 1.71 do
-    Q 'SELECT users.* FROM users WHERE users.id=?'
+    Q 'SELECT users.* FROM users WHERE users.id=? -- 29'
     Q 'SELECT comments_received.* FROM users.comments_received ' \
-      'WHERE users.id = ?'
-    Q 'SELECT from_user.nickname FROM comments.from_user WHERE comments.id = ?'
+      'WHERE users.id = ? -- 30'
+    Q 'SELECT from_user.nickname FROM comments.from_user WHERE comments.id = ? -- 31'
     Q 'SELECT bought_now.*, items.* FROM items.bought_now.buyer ' \
-      'WHERE buyer.id = ? AND bought_now.date>=?'
+      'WHERE buyer.id = ? AND bought_now.date>=? -- 32'
     Q 'SELECT items.* FROM items.seller WHERE seller.id=? AND ' \
-      'items.end_date >=?'
+      'items.end_date >=? -- 33'
     Q 'SELECT items.* FROM items.bids.user WHERE user.id=? AND ' \
-      'items.end_date>=?'
-
+      'items.end_date>=? -- 34'
   end
 end
 
