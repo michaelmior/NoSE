f692db7a3ae9b960124e41c3b9da77d921152ca0
Michael Mior <mmior@uwaterloo.ca>
2015-08-17 08:55:27 -0400

    Fix write_heavy workload

--- plans/rubis_expert.rb
+++ plans/rubis_expert.rb
@@ -3,7 +3,7 @@
 NoSE::Plans::ExecutionPlans.new do
   Schema 'rubis_expert'
 
-  Group 'BrowseCategories', browsing: 4.44 + 3.21, bidding: 7.65 + 5.39 do
+  Group 'BrowseCategories', browsing: 4.44 + 3.21, bidding: 7.65 + 5.39, write_heavy: 7.65 + 5.39 do
     Plan 'Authentication' do
       Select users.password
       Param  users.id, :==
@@ -17,7 +17,7 @@ NoSE::Plans::ExecutionPlans.new do
     end
   end
 
-  Group 'ViewBidHistory', browsing: 2.38, bidding: 1.54 do
+  Group 'ViewBidHistory', browsing: 2.38, bidding: 1.54, write_heavy: 1.54 do
     Plan 'ItemName' do
       Select items.name
       Param  items.id, :==
@@ -31,7 +31,7 @@ NoSE::Plans::ExecutionPlans.new do
     end
   end
 
-  Group 'ViewItem', browsing: 22.95, bidding: 14.17 do
+  Group 'ViewItem', browsing: 22.95, bidding: 14.17, write_heavy: 14.17 do
     Plan 'ItemData' do
       Select items['*']
       Param  items.id, :==
@@ -45,7 +45,7 @@ NoSE::Plans::ExecutionPlans.new do
     end
   end
 
-  Group 'SearchItemsByCategory', browsing: 27.77 + 8.26, bidding: 15.94 + 6.34 do
+  Group 'SearchItemsByCategory', browsing: 27.77 + 8.26, bidding: 15.94 + 6.34, write_heavy: 15.94 + 6.34 do
     Plan 'ItemList' do
       Select items['*']
       Param  categories.id, :==
@@ -61,7 +61,7 @@ NoSE::Plans::ExecutionPlans.new do
   # # SearchItemsByRegion
   # # BrowseRegions
 
-  Group 'ViewUserInfo', browsing: 4.41, bidding: 2.48 do
+  Group 'ViewUserInfo', browsing: 4.41, bidding: 2.48, write_heavy: 2.48 do
     Plan 'UserData' do
       Select users['*'], regions.name
       Param  users.id, :==
@@ -76,7 +76,7 @@ NoSE::Plans::ExecutionPlans.new do
     end
   end
 
-  Group 'RegisterItem', bidding: 0.53, write_heavy: 0.53 * 10 do
+  Group 'RegisterItem', bidding: 0.53, write_heavy: 0.53 * 100 do
     Plan 'InsertItem' do
       Param  items.id, :==
       Param  items.name, :==
@@ -107,7 +107,7 @@ NoSE::Plans::ExecutionPlans.new do
     end
   end
 
-  Group 'RegisterUser', bidding: 1.07, write_heavy: 1.07 * 10 do
+  Group 'RegisterUser', bidding: 1.07, write_heavy: 1.07 * 100 do
     Plan 'AddUser' do
       Support do
         Plan 'GetRegionName' do
@@ -140,7 +140,7 @@ NoSE::Plans::ExecutionPlans.new do
     # end
   end
 
-  Group 'BuyNow', bidding: 1.16 do
+  Group 'BuyNow', bidding: 1.16, write_heavy: 1.16 do
     Plan 'Authentication' do
       Select users.password
       Param  users.id, :==
@@ -154,7 +154,7 @@ NoSE::Plans::ExecutionPlans.new do
     end
   end
 
-  Group 'StoreBuyNow', bidding: 1.10, write_heavy: 1.10 * 10 do
+  Group 'StoreBuyNow', bidding: 1.10, write_heavy: 1.10 * 100 do
     Plan 'ReduceQuantity' do
       Support do
         Plan 'OldQuantity' do
@@ -178,7 +178,7 @@ NoSE::Plans::ExecutionPlans.new do
     end
   end
 
-  Group 'PutBid', bidding: 5.40 do
+  Group 'PutBid', bidding: 5.40, write_heavy: 5.40 do
     Plan 'Authentication' do
       Select users.password
       Param  users.id, :==
@@ -198,7 +198,7 @@ NoSE::Plans::ExecutionPlans.new do
     end
   end
 
-  Group 'StoreBid', bidding: 3.74, write_heavy: 3.74 * 10 do
+  Group 'StoreBid', bidding: 3.74, write_heavy: 3.74 * 100 do
     Plan 'AddBid' do
       Support do
         Plan 'GetMaxBid' do
@@ -227,7 +227,7 @@ NoSE::Plans::ExecutionPlans.new do
     end
   end
 
-  Group 'PutComment', bidding: 0.46 do
+  Group 'PutComment', bidding: 0.46, write_heavy: 0.46 do
     Plan 'Authentication' do
       Select users.password
       Param  users.id, :==
@@ -247,7 +247,7 @@ NoSE::Plans::ExecutionPlans.new do
     end
   end
 
-  Group 'StoreComment', bidding: 0.45, write_heavy: 0.45 * 10 do
+  Group 'StoreComment', bidding: 0.45, write_heavy: 0.45 * 100 do
     Plan 'UpdateRating' do
       Support do
         Plan 'GetRating' do
@@ -272,7 +272,7 @@ NoSE::Plans::ExecutionPlans.new do
     end
   end
 
-  Group 'AboutMe', bidding: 1.71 do
+  Group 'AboutMe', bidding: 1.71, write_heavy: 1.71 do
     Plan 'UserData' do
       Select users['*']
       Param  users.id, :==
