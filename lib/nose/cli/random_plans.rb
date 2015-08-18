d8650274c06801bea7037382e1f9c5673183706a
Michael Mior <mmior@uwaterloo.ca>
2015-08-18 16:15:35 -0400

    Add random query plan output

---
+++ lib/nose/cli/random_plans.rb
@@ -0,0 +1,47 @@
+module NoSE
+  module CLI
+    # Add a command to generate a graphic of the schema from a workload
+    class NoSECLI < Thor
+      desc 'random_plans WORKLOAD TAG',
+           'output random plans for the statement with TAG in WORKLOAD'
+      option :count, type: :numeric, default: 5, aliases: '-n'
+      option :mix, type: :string, default: 'default'
+      option :format, type: :string, default: 'txt',
+                      enum: %w(txt json yml), aliases: '-f'
+      option :output, type: :string, default: nil, aliases: '-o'
+      def random_plans(workload_name, tag)
+        workload = Workload.load workload_name
+        workload.mix = options[:mix].to_sym \
+          unless options[:mix] == 'default' && workload.mix != :default
+
+        # Find the statement with the given tag
+        statement = workload.statements.find do |s|
+          s.text.end_with? "-- #{tag}"
+        end
+
+        # Generate a random set of plans
+        indexes = IndexEnumerator.new(workload).indexes_for_workload
+        cost_model = get_class('cost', options[:cost_model][:name]).new(**options[:cost_model])
+        planner = Plans::QueryPlanner.new workload, indexes, cost_model
+        plans = planner.find_plans_for_query(statement).to_a
+        plans = plans.sample options[:count]
+
+        results = OpenStruct.new
+        results.workload = workload
+        results.enumerated_indexes = indexes
+        results.indexes = plans.map(&:indexes).flatten(1).to_set
+        results.plans = plans
+        results.cost_model = cost_model
+
+        # Output the results in the specified format
+        file = options[:output].nil? ? $stdout :
+                                       File.open(options[:output], 'w')
+        begin
+          send(('output_' + options[:format]).to_sym, results, file, true)
+        ensure
+          file.close unless options[:output].nil?
+        end
+      end
+    end
+  end
+end
