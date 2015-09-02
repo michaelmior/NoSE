130dd8ab201e877c6c48b3a7d5bc37566a76c630
Michael Mior <mmior@uwaterloo.ca>
2015-09-02 00:03:42 -0400

    Allow changing mix on reformat

--- lib/nose/cli/reformat.rb
+++ lib/nose/cli/reformat.rb
@@ -4,10 +4,15 @@ module NoSE
     class NoSECLI < Thor
       desc 'reformat PLAN_FILE',
            'reformat the data in PLAN_FILE'
+      option :mix, type: :string, default: 'default',
+                   banner: 'the name of the workload mix for weighting queries'
       option :format, type: :string, default: 'txt',
                       enum: %w(txt json yml), aliases: '-f'
       def reformat(plan_file)
         result = load_results plan_file
+        result.workload.mix = options[:mix].to_sym \
+          unless options[:mix] == 'default' && result.workload.mix != :default
+        result.recalculate_cost
 
         # Output the results in the specified format
         send(('output_' + options[:format]).to_sym, result)
