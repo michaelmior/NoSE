e6071f6b3d2892b881ad1ae5997b19f45cd11ad7
Michael Mior <mmior@uwaterloo.ca>
2016-01-13 16:17:25 -0500

    Refactor CLI "why" command

--- lib/nose/cli/why.rb
+++ lib/nose/cli/why.rb
@@ -13,37 +13,51 @@ module NoSE
 
       def why(plan_file)
         result = load_results plan_file
-
-        query_count = Set.new
-        support_count = Set.new
         indexes_usage = Hash.new { |h, k| h[k] = [] }
 
         # Count the indexes used in queries
-        result.plans.each do |plan|
-          plan.indexes.each do |index|
-            indexes_usage[index] << plan.query
-            query_count.add index
-          end
-        end
+        query_count = Set.new
+        update_index_usage result.plans, indexes_usage, query_count
 
         # Count the indexes used in support queries
+        # (ignoring those used in queries)
+        support_count = Set.new
         result.update_plans.each do |plan|
-          plan.query_plans.each do |query_plan|
-            query_plan.indexes.each do |index|
-              indexes_usage[index] << plan.statement
-              support_count.add index unless query_count.include? index
-            end
-          end
+          update_index_usage plan.query_plans, indexes_usage,
+                             support_count, query_count
         end
 
-        # Print out the statements each index is used for
+        # Produce the final output of index usage
+        print_index_usage indexes_usage, query_count, support_count
+      end
+
+      private
+
+      # Track usage of indexes in the set of query plans updating both
+      # a dictionary of statements relevant to each index and a set
+      # of unique statements used (optionally ignoring some)
+      def update_index_usage(plans, indexes_usage, statement_usage,
+                             ignore = Set.new)
+        plans.each do |plan|
+          plan.indexes.each do |index|
+            if plan.respond_to?(:statement)
+              indexes_usage[index] << plan.statement
+            else
+              indexes_usage[index] << plan.query
+            end
+            statement_usage.add index unless ignore.include? index
+          end
+        end
+      end
+
+      # Print out the statements each index is used for
+      def print_index_usage(indexes_usage, query_count, support_count)
         indexes_usage.each do |index, statements|
           p index
           statements.each { |s| p s }
           puts
         end
 
-        # Put the number of indexes used for queries and support queries
         puts "        Queries: #{query_count.length}"
         puts "Support queries: #{support_count.length}"
       end
