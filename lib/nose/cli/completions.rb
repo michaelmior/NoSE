31d0f55c31484491f1dfd927ff6af3dfd830b418
Michael Mior <mmior@uwaterloo.ca>
2016-06-14 20:01:00 -0400

    Add generation of a shell completion script

---
+++ lib/nose/cli/completions.rb
@@ -0,0 +1,25 @@
+require 'erb'
+require 'shellwords'
+
+module NoSE
+  module CLI
+    # Add a command to generate a shell completion script for nose
+    class NoSECLI < Thor
+      desc 'completions', 'generate a shell completion script for nose'
+
+      long_desc <<-LONGDESC
+        `nose completions` generates a shell script which can be sourced to
+        autocomplete the commands and options provided by nose.
+      LONGDESC
+
+      def completions
+        commands = NoSE::CLI::NoSECLI.all_commands.to_a.sort_by(&:first)
+
+        tmpl = File.read File.join(File.dirname(__FILE__),
+                                   '../../../completions.erb')
+        ns = OpenStruct.new commands: commands
+        puts ERB.new(tmpl, nil, '>').result(ns.instance_eval { binding })
+      end
+    end
+  end
+end
