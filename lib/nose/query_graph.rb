3a11ce0a2d1611273fb788b20648704aedb4d94a
Michael Mior <mmior@uwaterloo.ca>
2016-06-28 14:37:13 -0400

    Add method to generate subgraphs from graphs

--- lib/nose/query_graph.rb
+++ lib/nose/query_graph.rb
@@ -9,6 +9,20 @@ module NoSE
       def initialize(entity)
         @entity = entity
       end
+
+      def inspect
+        @entity.name
+      end
+
+      # Two nodes are equal if they represent the same entity
+      def ==(other)
+        other.is_a?(Node) && @entity == other.entity
+      end
+      alias eql? ==
+
+      def hash
+        @entity.name.hash
+      end
     end
 
     class Edge
@@ -19,29 +33,137 @@ module NoSE
         @to = to
         @key = key
       end
+
+      def inspect
+        @key.inspect
+      end
+
+      def ==(other)
+        return false unless other.is_a? Edge
+        canonical_params == other.canonical_params
+      end
+      alias eql? ==
+
+      def hash
+        canonical_params.hash
+      end
+
+      # Produce the parameters to initialize the canonical version of this edge
+      def canonical_params
+        if @from.entity.name > @to.entity.name
+          [@from.entity.name, @to.entity.name, @key.name]
+        else
+          [@to.entity.name, @from.entity.name, @key.reverse.name]
+        end
+      end
     end
 
     # A graph identifying entities and relationships involved in a query
     class Graph
-      def initialize
+      attr_reader :root, :nodes
+
+      def initialize(root = nil, *edges)
         @nodes = Set.new
+        @root = add_node root unless root.nil?
         @edges = Hash.new { |h, k| h[k] = Set.new }
+
+        edges.each { |edge| add_edge(*edge) }
+      end
+
+      def inspect
+        "Graph(root: #{@root.inspect}, " \
+              "nodes: #{@nodes.map(&:inspect).join(', ')}, " \
+              "edges: #{@edges.inspect})"
+      end
+
+      def ==(other)
+        return false unless other.is_a? Graph
+        @root == other.root && @nodes == other.nodes &&
+          @edges = other.instance_variable_get(:@edges)
+      end
+      alias eql? ==
+
+      def hash
+        [@root, @nodes, @edges].hash
+      end
+
+      # Produce all entities contained in this graph
+      def entities
+        @nodes.map(&:entity).to_set
       end
 
       # Add a new node to the graph
-      def add_node(entity)
-        node = Node.new entity
+      def add_node(node)
+        node = Node.new node if node.is_a? Entity
         @nodes.add node
         node
       end
 
       # Add a new edge betwene two nodes in the graph
       def add_edge(node1, node2, key)
-        @nodes.add node1
-        @edges[node1].add Edge.new(node1, node2, key)
+        node1 = add_node node1
+        node2 = add_node node2
 
-        @nodes.add node2
+        @edges[node1].add Edge.new(node1, node2, key)
         @edges[node2].add Edge.new(node2, node1, key.reverse)
+      end
+
+      # Prune nodes not reachable from the root
+      def prune
+        fail 'Cannot prune without root' if @root.nil?
+        to_visit = [@root]
+        reachable = Set.new([@root])
+
+        # Determine which nodes are reachable from the root
+        until to_visit.empty?
+          discovered = Set.new
+          to_visit.each do |node|
+            discovered += @edges[node].map(&:to).to_set
+          end
+          to_visit = discovered - reachable
+          reachable += discovered
+        end
+
+        # Remove any nodes and edges which are not reachable
+        @edges.select! { |node| reachable.include? node }
+        @edges.each do |_, edges|
+          edges.select! do |edge|
+            reachable.include?(edge.to) && reachable.include?(edge.from)
+          end
+        end
+        @edges.reject! { |_, edges| edges.empty? }
+        @nodes = reachable
+      end
+
+      # Produce an enumerator which yields all subgraphs of this graph
+      def subgraphs(recursive = true)
+        # We have no subgraphs if there is only one node
+        return [] if @nodes.size == 1
+
+        # Construct a list of all unique edges in the graph
+        all_edges = @edges.values.reduce(&:union).to_a.uniq(&:canonical_params)
+
+        all_subgraphs = Set.new
+        all_edges.each do |remove_edge|
+          # Construct new graphs rooted at either side of the cut edge
+          graph1 = Graph.new(remove_edge.from)
+          graph2 = Graph.new(remove_edge.to)
+          (all_edges - [remove_edge]).each do |edge|
+            graph1.add_edge edge.from, edge.to, edge.key
+            graph2.add_edge edge.from, edge.to, edge.key
+          end
+
+          # Prune the graphs before yielding them and their subgraphs
+          graph1.prune
+          all_subgraphs.add graph1
+          all_subgraphs += graph1.subgraphs if recursive
+
+          graph2.prune
+          all_subgraphs.add graph2
+          all_subgraphs += graph2.subgraphs if recursive
+        end
+
+        all_subgraphs.to_set
       end
 
       # Output an image of the query graph
---
+++ spec/query_graph_spec.rb
@@ -0,0 +1,35 @@
+module NoSE
+  module QueryGraph
+    describe QueryGraph do
+      include_context 'entities'
+
+      it 'produces no subgraphs for a single entity graph' do
+        graph = Graph.new(user)
+        expect(graph.subgraphs).to be_empty
+      end
+
+      it 'produces single node subgraphs when splitting with two nodes' do
+        graph = Graph.new nil, [user, tweet, user['Tweets']]
+        subgraphs = graph.subgraphs.to_a
+        expect(subgraphs).to match_array [
+          Graph.new(user),
+          Graph.new(tweet)
+        ]
+      end
+
+      it 'produces all paths when splitting a graph' do
+        graph = Graph.new nil,
+                          [user, tweet, user['Tweets']],
+                          [tweet, link, tweet['Link']]
+        subgraphs = graph.subgraphs.to_a
+        expect(subgraphs).to match_array [
+          Graph.new(user),
+          Graph.new(tweet),
+          Graph.new(link),
+          Graph.new(tweet, [user, tweet, user['Tweets']]),
+          Graph.new(tweet, [tweet, link, tweet['Link']])
+        ]
+      end
+    end
+  end
+end
