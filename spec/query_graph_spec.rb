c206249a2a80f0a5e9afb1c30e49e7b7ea160d8d
Michael Mior <mmior@uwaterloo.ca>
2016-06-29 06:45:40 -0400

    Add context to query graph test

--- spec/query_graph_spec.rb
+++ spec/query_graph_spec.rb
@@ -3,32 +3,34 @@ module NoSE
     describe QueryGraph do
       include_context 'entities'
 
-      it 'produces no subgraphs for a single entity graph' do
-        graph = Graph.new(user)
-        expect(graph.subgraphs).to be_empty
-      end
+      context 'when producing subgraphs' do
+        it 'produces nothing for a single entity graph' do
+          graph = Graph.new(user)
+          expect(graph.subgraphs).to be_empty
+        end
 
-      it 'produces single node subgraphs when splitting with two nodes' do
-        graph = Graph.new nil, [user, tweet, user['Tweets']]
-        subgraphs = graph.subgraphs.to_a
-        expect(subgraphs).to match_array [
-          Graph.new(user),
-          Graph.new(tweet)
-        ]
-      end
+        it 'produces single node graphs when splitting with two nodes' do
+          graph = Graph.new nil, [user, tweet, user['Tweets']]
+          subgraphs = graph.subgraphs.to_a
+          expect(subgraphs).to match_array [
+            Graph.new(user),
+            Graph.new(tweet)
+          ]
+        end
 
-      it 'produces all paths when splitting a graph' do
-        graph = Graph.new nil,
-                          [user, tweet, user['Tweets']],
-                          [tweet, link, tweet['Link']]
-        subgraphs = graph.subgraphs.to_a
-        expect(subgraphs).to match_array [
-          Graph.new(user),
-          Graph.new(tweet),
-          Graph.new(link),
-          Graph.new(tweet, [user, tweet, user['Tweets']]),
-          Graph.new(tweet, [tweet, link, tweet['Link']])
-        ]
+        it 'produces all paths when splitting a graph' do
+          graph = Graph.new nil,
+                            [user, tweet, user['Tweets']],
+                            [tweet, link, tweet['Link']]
+          subgraphs = graph.subgraphs.to_a
+          expect(subgraphs).to match_array [
+            Graph.new(user),
+            Graph.new(tweet),
+            Graph.new(link),
+            Graph.new(tweet, [user, tweet, user['Tweets']]),
+            Graph.new(tweet, [tweet, link, tweet['Link']])
+          ]
+        end
       end
     end
   end
