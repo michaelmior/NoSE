509283cf307df9ef94462c51a4be1a5e1d31305d
Michael Mior <mmior@uwaterloo.ca>
2016-08-05 23:43:14 -0400

    Generalize debugging code

--- lib/nose/debug.rb
+++ lib/nose/debug.rb
@@ -4,13 +4,13 @@ require 'pry'
 module NoSE
   # Various helpful debugging snippets
   module Debug
-    # Convenience method to break in IndexLookupStep#apply
-    # when a particular set of indexes is reach when planning
+    # Convenience method to break in IndexLookupStep when
+    # a particular set of indexes is reach when planning
     # @return [void]
     def self.break_on_indexes(*index_keys)
       apply = binding.of_caller(1)
-      parent = apply.local_variable_get :parent
-      index = apply.local_variable_get :index
+      parent = apply.eval 'parent'
+      index = apply.eval 'index'
       current_keys = parent.parent_steps.indexes.map(&:key) << index.key
 
       # rubocop:disable Lint/Debugger
