e56411cc2aa9a3c77bee319471721450436be101
Michael Mior <mmior@uwaterloo.ca>
2016-09-02 13:21:00 -0400

    Clean up field size cost model

--- lib/nose/cost/field_size.rb
+++ lib/nose/cost/field_size.rb
@@ -9,14 +9,10 @@ module NoSE
       # Rough cost estimate as the size of data returned
       # @return [Numeric]
       def index_lookup_cost(step)
-        if step.state.answered?
-          # If we have an answer to the query, we only need
-          # to fetch the data fields which are selected
-          fields = step.index.hash_fields + step.index.order_fields +
-                   step.index.extra & step.state.query.select
-        else
-          fields = step.index.all_fields
-        end
+        # If we have an answer to the query, we only need
+        # to fetch the data fields which are selected
+        fields = step.index.all_fields
+        fields &= step.state.query.select if step.state.answered?
 
         step.state.cardinality * fields.sum_by(&:size)
       end
